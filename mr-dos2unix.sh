f7d61f3de7ed506f4876ee5b399f3a3b6b694bcc
hubte1g <lickteig.joshua@gmail.com>
2016-07-13 08:57:53 -0500

    Update mr-dos2unix.sh

--- mr-dos2unix.sh
+++ mr-dos2unix.sh
@@ -1,7 +1,7 @@
-# in dir, remove controlling characters present after copying from Windows to Unix 
+# in dir, remove controlling characters present after copying from Windows to Unix ; apt-get install dos2unix || yum install dos2unix
 dos2unix *
 
-#  change all file permissions for shell scripts
+#  change all file permissions for shell scripts, will apply it to all flags: [u]ser, [g]roup, [o]thers
 chmod +x *.sh
 
 # compile
