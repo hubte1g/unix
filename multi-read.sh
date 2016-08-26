962d81c5e6891972b50b0d0df996fc2a7fcb29f5
hubte1g <lickteig.joshua@gmail.com>
2016-08-25 19:29:23 -0500

    Update multi-read.sh

--- multi-read.sh
+++ multi-read.sh
@@ -1,3 +1,3 @@
 for i in $(cat list.txt); do paste $i | head -2; 
 
-for i in $(cat list.txt); do echo $i; paste $i | head -2; echo '\n'; done 
+for i in $(cat list.txt); do echo "File: "$i; paste $i | head -2; echo '\n'; done 
